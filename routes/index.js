8469fea4524e6f81b6140b8f332b6128c36d6b86
petrKantek <57535586+petrKantek@users.noreply.github.com>
2023-04-05 19:29:48 +0200

    add adtional API endpoints

--- routes/index.js
+++ routes/index.js
@@ -5,4 +5,10 @@ const router = express.Router();
 
 router.get("/api/notes", noteController.getNotes);
 
+router.post("/api/add-note", noteController.addNote);
+
+router.delete("/api/delete-note/:id", noteController.deleteNote);
+
+router.get("/api/notes/count", noteController.numNotes);
+
 module.exports = router;
