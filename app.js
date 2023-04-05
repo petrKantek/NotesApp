fc38307faf63cd42b13f0ee097fd90121796789e
petrKantek <57535586+petrKantek@users.noreply.github.com>
2023-04-05 19:49:56 +0200

    add swagger OpenAPI docs

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const path = require("path");
 const noteRoutes = require("./routes/index");
 const errorMiddleware = require("./middlewares/error");
 const cors = require("cors");
+const swaggerUi = require('swagger-ui-express')
+const swaggerFile = require('./swagger_output.json')
 
 const app = express();
 app.use(express.urlencoded({ extended: false }));
@@ -11,6 +13,8 @@ app.use(express.json());
 app.use(cors());
 
 app.use(noteRoutes);
+
+app.use('/doc', swaggerUi.serve, swaggerUi.setup(swaggerFile))
 
 app.use("*", (req, res) => {
   res.status(404).json({
